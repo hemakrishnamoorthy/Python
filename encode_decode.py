732b11c510f927f32134d39524bae3fc7e66c494
hemakrishnamoorthy <60175378+hemakrishnamoorthy@users.noreply.github.com>
2023-10-28 13:08:11 -0500

    Update encode_decode.py

--- encode_decode.py
+++ encode_decode.py
@@ -9,31 +9,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
-    if direction == "encode":
-        encrypted_text = ""
-        for i in range(len(text)):
-            idx = alphabet.index(text[i])
+def cypher(text, shift, direction):
+    cypher_text = ""
+    for i in range(len(text)):
+        idx = alphabet.index(text[i])
+        if direction == 'encode':
             new_idx = idx + shift
-
-            encrypted_text += alphabet[new_idx]
-
-        print(f"Your encoded message is {encrypted_text}")
-
-
-def decrypt(text, shift):
-    if direction == "decode":
-        decrypted_text = ""
-        for i in range(len(text)):
-            idx = alphabet.index(text[i])
+        elif direction == 'decode':
             new_idx = idx - shift
+        cypher_text += alphabet[new_idx]
 
-            decrypted_text += alphabet[new_idx]
-
-        print(f"Your decoded message is {decrypted_text}")
+    print(f"Your {direction}d message is {cypher_text}")
 
 
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
+cypher(text, shift, direction)
